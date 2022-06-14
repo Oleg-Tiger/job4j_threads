2a3f1344038d9976397e373398df13737f13418b
Oleg-Tiger <free_person@inbox.ru>
2022-06-14 22:43:49 +0300

    0. CAS - операции   [#6859]

---
+++ src/main/java/ru/job4j/CASCount.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+import net.jcip.annotations.ThreadSafe;
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private  final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int countBeforeIncrement;
+        do {
+            countBeforeIncrement = count.get();
+        } while (!count.compareAndSet(countBeforeIncrement, countBeforeIncrement++));
+    }
+
+    public int get() {
+     return count.get();
+    }
+}
