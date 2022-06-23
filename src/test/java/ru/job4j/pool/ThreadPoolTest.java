1e7dca9d2bb2ecd76869e7c4b9edb946dd7f9d06
Oleg-Tiger <free_person@inbox.ru>
2022-06-23 14:21:31 +0300

    1. Реализовать ThreadPool [#1099]. Refactoring.

--- src/test/java/ru/job4j/pool/ThreadPoolTest.java
+++ src/test/java/ru/job4j/pool/ThreadPoolTest.java
@@ -2,22 +2,18 @@ package ru.job4j.pool;
 
 import org.junit.Test;
 
-import java.util.concurrent.atomic.AtomicReference;
-
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ThreadPoolTest {
-    private static final AtomicReference<Integer> COUNT = new AtomicReference<>(0);
+    private static int count = 0;
 
     private class RunnableForPool implements Runnable {
 
         @Override
         public synchronized void run() {
-            int countNow;
-            do {
-                countNow = COUNT.get();
-            } while (!COUNT.compareAndSet(countNow, countNow + 1));
+            count++;
+            count++;
         }
     }
 
@@ -30,6 +26,6 @@ public class ThreadPoolTest {
         Thread.sleep(500);
         threadPool.shutdown();
         Thread.sleep(500);
-        assertThat(COUNT.get(), is(3));
+        assertThat(count, is(6));
     }
 }
