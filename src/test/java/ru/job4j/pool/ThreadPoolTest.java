1e10b3f6b978247b0f2f70f7cd91795745419576
Oleg-Tiger <free_person@inbox.ru>
2022-06-26 23:14:20 +0300

    1. Реализовать ThreadPool [#1099 #313555]

--- src/test/java/ru/job4j/pool/ThreadPoolTest.java
+++ src/test/java/ru/job4j/pool/ThreadPoolTest.java
@@ -1,19 +1,19 @@
 package ru.job4j.pool;
 
 import org.junit.Test;
-
+import ru.job4j.Count;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ThreadPoolTest {
-    private static int count = 0;
+    private static final Count COUNT = new Count();
 
     private class RunnableForPool implements Runnable {
 
         @Override
-        public synchronized void run() {
-            count++;
-            count++;
+        public void run() {
+            COUNT.increment();
+            COUNT.increment();
         }
     }
 
@@ -26,6 +26,6 @@ public class ThreadPoolTest {
         Thread.sleep(500);
         threadPool.shutdown();
         Thread.sleep(500);
-        assertThat(count, is(6));
+        assertThat(COUNT.get(), is(6));
     }
 }
